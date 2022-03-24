6616f54fdf6f9355ceba734824bd0bc89258cf62
cagiraldoa <cagiraldoa@eaft.edu.co>
2022-03-24 00:36:27 -0500

    404

--- src/pages/not-found-page/not-found-page.js
+++ src/pages/not-found-page/not-found-page.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Image } from "mui-image";
+import Styles from "./not-found-page.module.scss";
+import { Text } from "../../components/text/text";
+import { MainButton } from "../../components/general/button/main-button";
 
-function NotFoundPage  () {
+
+function NotFoundPage() {
+  const Image404 = require("../../../src/images/404.png");
+  
+  const tuyaImage = require("../../images/tuya.png");
   return (
-    <div>
-      <h1>404</h1>
+    <div className={Styles.homeGrid}>
+      <div className={Styles.homeGrid__imgWrapper}>
+        <Image src={tuyaImage}/>
+        <div className={Styles.texto}>
+        <Text className={Styles.texto__text1}>¡Lo sentimos!</Text>
+        <Text className={Styles.texto__text2}>No hemos podido encontrar la pagina que buscabas</Text>
+        </div>
+        <MainButton className={Styles.texto__boton}> Regresa al inicio </MainButton>
+      </div>
+      <div className={Styles.homeGrid__404}>
+      <Image src={Image404}/>
+      </div>
     </div>
   );
-};
+}
 
 export { NotFoundPage };
