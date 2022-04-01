a54f811f2d01972cd4e3883667a177e0b0ad0e23
cagiraldoa <cagiraldoa@eaft.edu.co>
2022-04-01 16:22:34 -0500

    Sizing modified

--- src/pages/not-found-page/not-found-page.js
+++ src/pages/not-found-page/not-found-page.js
@@ -4,23 +4,27 @@ import Styles from "./not-found-page.module.scss";
 import { Text } from "../../components/text/text";
 import { MainButton } from "../../components/general/button/main-button";
 
-
 function NotFoundPage() {
   const Image404 = require("../../../src/images/404.png");
-
   const tuyaImage = require("../../images/tuya.png");
   return (
     <div className={Styles.homeGrid}>
-      <div className={Styles.homeGrid__imgWrapper}>
-        <Image src={tuyaImage}/>
-        <div className={Styles.texto}>
-        <Text className={Styles.texto__text1}>¡Lo sentimos!</Text>
-        <Text className={Styles.texto__text2}>No hemos podido encontrar la pagina que buscabas</Text>
+      <div className={Styles.homeGrid__content}>
+        <div className={Styles.imgWrapper}>
+          <Image src={tuyaImage} />
         </div>
-        <MainButton href="/" className={Styles.texto__boton}> Regresa al inicio </MainButton>
+        
+        <Text marginBottom = "4%" fontSize="3rem" fontWeight="900" >¡Lo sentimos!</Text>
+        <Text marginBottom = "6%" fontSize="1.1rem" fontWeight="500" >
+          No hemos podido encontrar la pagina que buscabas
+        </Text>
+        
+        <MainButton width="70%" href="/">          
+          Regresa al inicio
+        </MainButton>
       </div>
-      <div className={Styles.homeGrid__404}>
-      <Image src={Image404}/>
+      <div className={Styles.homeGrid__img404}>
+        <Image src={Image404} />
       </div>
     </div>
   );
