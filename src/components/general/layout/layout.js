456b02ae00642a6a64b7a82b6bb67c23bb8f0798
afernander <afernander@eafit.edu.co>
2022-05-24 11:31:43 -0500

    login fetch

--- src/components/general/layout/layout.js
+++ src/components/general/layout/layout.js
@@ -25,7 +25,7 @@ import { Text } from "../../text/text";
 import Image from "./../../../images/girl.png";
 import LayoutBg from "./../../../images/bg-layout.png";
 import { Button } from "@mui/material";
-
+import {useState, useEffect} from 'react';
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 
@@ -111,6 +111,24 @@ export default function Aside(props) {
     console.log(filter);
   };
 
+  const handleName = async () => {
+
+    fetch("http://localhost:3000/users/whoiam", {
+      "method": "GET",
+      "headers": {
+        "cookie": "session=eyJ1c2VySWQiOjR9; session.sig=zkXT5HoUJr-HOXcWpnfVKBoJMZ4"
+      }
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+ 
+  
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -180,7 +198,7 @@ export default function Aside(props) {
               src={Image}
               sx={{ width: 160, height: 160, marginTop: "1rem" }}
             />
-            <Text color="white"> JUANITA PEREZ</Text>
+            <Text color="white">juana</Text>
             <Text color="white"> Universidad EAFIT</Text>
           </Box>
           <IconButton onClick={handleDrawerClose}>
